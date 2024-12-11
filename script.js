4eb2323be050d2c2bf418ddad1f077cb9071e534
clubmbusan <clubmbusan@naver.com>
2024-12-11 16:05:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,9 @@ addAssetButton.addEventListener('click', () => {
 
     // 모든 재산의 합계 계산
     const totalAssetValue = assets.reduce((sum, asset) => sum + asset.value, 0);
-
+result.innerHTML = `<p>총 재산 금액: ${totalAssetValue.toLocaleString()} 원</p>`;
+    });
+});
     // 개인분 또는 전체분 계산
     if (inheritanceType.value === 'personal') {
         const relationship = document.getElementById('relationshipPersonal').value;
